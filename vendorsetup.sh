f588b319af5da183e3a60028e916bb7a7051b0d6
TopexGuy <topexguy@gmail.com>
2026-04-29 14:54:37 +0200

    extras: depth cloning vanadium

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://gitlab.com/grapheneos/platform_external_vanadium.git external/vanadium
+git clone --depth=1 https://gitlab.com/grapheneos/platform_external_vanadium.git external/vanadium
 
 echo "[*] merging splitted apps..."
 bash vendor/extras/merge_splits.sh
